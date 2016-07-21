7f8e08c86b9298fdb86a32c0cbacd3b887add012
Admin <admin@LIB2-49.local>
2016-07-21 12:09:54 -0400

    Template for movie detail

--- movielist/views.py
+++ movielist/views.py
@@ -6,11 +6,13 @@ from django.http import HttpResponse
 
 from movielist.models import Movie
 
+from django.template import Context, loader
+
 def show_movie(request):
-	m = Movie.objects.all()
-
-	text = ''
-	for k in m:
-		text += '<p>' + k.title + '</p>'
-
-	return HttpResponse(text)
+	movie_list = Movie.objects.all()
+	template = loader.get_template(
+		'movielist/movie_detail.html')
+	# Only take the first movie.
+	context = Context({'m': movie_list[0]})
+	output = template.render(context)
+	return HttpResponse(output)
