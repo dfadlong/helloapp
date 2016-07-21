434d1fbf68bc2f8a02c149bc32a65a7f2e86bc24
Admin <admin@LIB2-49.local>
2016-07-21 13:35:04 -0400

    Get movies by year, paginate results

--- movielist/views.py
+++ movielist/views.py
@@ -9,10 +9,22 @@ from movielist.models import Movie
 from django.template import Context, loader
 
 def show_movie(request):
-	movie_list = Movie.objects.all()
+	year = request.GET.get('year', '')
+	page = int(request.GET.get('page', '0'))
+
+	if year:
+		movie_list = Movie.objects.filter(year=year)
+	else:
+		movie_list = Movie.objects.all()
+	num = len(movie_list)
 	template = loader.get_template(
 		'movielist/movie_detail.html')
 	# Only take the first movie.
-	context = Context({'movies': movie_list[:10]})
+	context = {
+		'movies': movie_list[page * 10: page * 10 + 10],
+		'num': num,
+		'year': year,
+		'next_page': page + 1,
+	}
 	output = template.render(context)
 	return HttpResponse(output)
